1fee262557c2a92880d35a05ed6d2f93fc072867
ruliworst <raulfa1503@gmail.com>
2023-01-31 22:24:07 +0100

    OrdersNavigationBar component modelled.

---
+++ src/components/OrdersNavigationBar.js
@@ -0,0 +1,13 @@
+const OrdersNavigationBar = () => {
+  return <div className="OrdersNavigationBar">
+    <h3>Orders</h3>
+    <ul>
+        <li>Pending</li>
+        <li>Delivered</li>
+        <li>Cancelled</li>
+        <li>Returned</li>
+      </ul>
+  </div>
+}
+
+export default OrdersNavigationBar;
