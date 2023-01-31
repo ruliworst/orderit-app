4b7e7435e19529c47a2cdbd6df218b7863470950
ruliworst <raulfa1503@gmail.com>
2023-01-31 22:27:51 +0100

    Modelling App component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import OrdersTable from './components/OrdersTable';
+import OrdersNavigationBar from './components/OrdersNavigationBar';
+import OrderForm from './components/OrderForm';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <h1>Order It!</h1>
+      <p>The app for managing all your e-commerce orders.</p>
+      <OrdersNavigationBar />
+      <OrderForm />
+      <OrdersTable />
     </div>
   );
 }
