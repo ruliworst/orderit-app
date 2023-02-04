09ad0244f970cb07ba77a9fb26695b9a99b05958
ruliworst <raulfa1503@gmail.com>
2023-02-04 14:28:19 +0100

    Extracting OrderForm from App component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import OrdersTable from './components/OrdersTable';
 import OrdersNavigationBar from './components/OrdersNavigationBar';
-import OrderForm from './components/OrderForm';
 
 const App = () => {
   return (
@@ -9,7 +8,6 @@ const App = () => {
       <h1>Order It!</h1>
       <p>The app for managing all your e-commerce orders.</p>
       <OrdersNavigationBar />
-      <OrderForm />
       <OrdersTable />
     </div>
   );
