d2618467ab8ee6eb3bf590e8de2645596a8016a5
ruliworst <raulfa1503@gmail.com>
2023-02-09 20:03:40 +0100

    New order functionality added.

--- src/components/OrdersNavigationBar.js
+++ src/components/OrdersNavigationBar.js
@@ -1,12 +1,25 @@
-const OrdersNavigationBar = () => {
+import '../styles/OrdersNavigationBar.css'
+import Togglable from './Togglable'
+import OrderForm from './OrderForm'
+import { useRef } from 'react'
+
+const OrdersNavigationBar = (props) => {
+  const { createOrder } = props
+
+  const createOrderFormRef = useRef()
+
   return <div className="OrdersNavigationBar">
     <h3>Orders</h3>
     <ul>
-        <li>Pending</li>
-        <li>Delivered</li>
-        <li>Cancelled</li>
-        <li>Returned</li>
-      </ul>
+      <li>All</li>
+      <li>Pending</li>
+      <li>Delivered</li>
+      <li>Cancelled</li>
+      <li>Returned</li>
+    </ul>
+    <Togglable buttonLabel="New Order" ref={createOrderFormRef}>
+      <OrderForm createOrder={createOrder}/>
+    </Togglable>
   </div>
 }
 
